6983a988f8ea1ceb20fddab9f156c052910dfa98
zhyrgal <zhyrgal@10.1.10.157>
2018-03-15 17:14:03 -0500

    Change from Zhyrgal

--- src/test/java/utilities/Browser.java
+++ src/test/java/utilities/Browser.java
@@ -4,6 +4,6 @@ public class Browser {
 	public static void main(String[] args) {
 		System.out.println("First code");
 		System.out.println("Kandai Jyrgal");
-		
+		System.out.println("Jakwy Makesh baike");
 	}
 }
