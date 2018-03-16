e32b456cff6b70d644d9b7aaac789b66d9c69c08
NURGIZA <NURGIZA@10.1.10.144>
2018-03-15 19:29:38 -0500

    M

--- src/test/java/utilities/Browser.java
+++ src/test/java/utilities/Browser.java
@@ -1,21 +1,11 @@
 package utilities;
 
+import javax.sound.midi.Soundbank;
+
 public class Browser {
 	public static void main(String[] args) {
 		System.out.println("First code");
 		System.out.println("Kandai Jyrgal");
-		System.out.println("Jakwy Makesh baike");
-		System.out.println("Hi Mira");
-		System.out.println("Topa good girl");
-		
-		System.out.println("Real Champion");
-
-		System.out.println("MAYRAMBEK");	
-		System.out.println("TOPA p");
-		System.out.println("My new chan");
-		System.out.println("dfgkfsdjgh");
-		
-	System.out.println("fdjkghfkjg");
-
+		System.out.println("++++++++++++++++");
 	}
 }
