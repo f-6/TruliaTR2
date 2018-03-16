3fea2f9727ff0b34bb6020337a117f52b7a2091b
asekovkanatbek <asekovkanatbek@Asekov>
2018-03-15 22:29:50 -0600

    topa commit browser, config and properties

--- src/test/java/utilities/Browser.java
+++ src/test/java/utilities/Browser.java
@@ -1,13 +1,55 @@
 package utilities;
 
-import javax.sound.midi.Soundbank;
+import java.nio.channels.NetworkChannel;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Browser {
-	public static void main(String[] args) {
-		System.out.println("First code");
-		System.out.println("Kandai Jyrgal");
-		System.out.println("Makew changes");
-		System.out.println("tala comp");
-		System.out.println("Fantastic 6 is the Best of the best");
+	private static WebDriver driver;
+	
+	public static WebDriver getDriver() {
+		int iWait = Integer.parseInt(Config.getProperty("implicitlyWait"));
+	
+		if(driver==null) {
+			switch(Config.getProperty("browser").toLowerCase()) {
+			case "chrome":
+				WebDriverManager.chromedriver().setup();
+				driver = new ChromeDriver();
+				driver.manage().timeouts().implicitlyWait(iWait, TimeUnit.SECONDS);
+				return driver;
+			case "firefox":
+				WebDriverManager.firefoxdriver().setup();
+				driver = new FirefoxDriver();
+				driver.manage().timeouts().implicitlyWait(iWait, TimeUnit.SECONDS);
+				return driver;
+			default:
+				WebDriverManager.chromedriver().setup();
+				driver = new ChromeDriver();
+				driver.manage().timeouts().implicitlyWait(iWait, TimeUnit.SECONDS);
+				return driver;
+			}
+			
+			
+		}
+		
+		return driver;
+	}
+	
+	public static void sleep(int seconds) {
+		try {
+			Thread.sleep(seconds*1000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void quit() {
+		sleep(5);
+		driver.quit();
 	}
 }
--- src/test/java/utilities/Config.java
+++ src/test/java/utilities/Config.java
@@ -1,5 +1,24 @@
 package utilities;
 
-public class Config {
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
 
+public class Config {
+	private static Properties properties;
+	
+	static {
+		try {
+			String pPath = ".//src/test/resources/testData/configuration.properties";
+			FileInputStream input = new FileInputStream(pPath);
+			properties = new Properties();
+			properties.load(input);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static String getProperty(String key) {
+		return properties.getProperty(key);
+	}
 }
