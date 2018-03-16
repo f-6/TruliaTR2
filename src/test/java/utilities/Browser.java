f9754e44c821012f1cba59ffd65db1fe8c8407d8
ulanbekalym <ulanbekalym@10.1.10.169>
2018-03-15 19:21:18 -0500

    ulan's version

--- src/test/java/utilities/Browser.java
+++ src/test/java/utilities/Browser.java
@@ -14,6 +14,7 @@ public class Browser {
 		System.out.println("TOPA p");
 		System.out.println("My new chan");
 		System.out.println("dfgkfsdjgh");
+		System.out.println("Ulanbek's changes");
 
 	}
 }
